b18fcd148f46267485e12662dedc46efa4bdd2a1
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-02-09 09:11:20 -0600

    created my node document with node modules and json packages. linked Inquirer in js doc.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+//open source code libary with a collection of common interactive command line user interfaces.
+var inquirer = require("inquirer");
+//An API that gives code to navigate the file system in node
+var fs = require('fs');
+
+
+
+
+//Photo of person
+
+//User Name
+
+//User websites and social media:
+    //User's location via Google Maps
+
+    //User GitHub Profile
+
+    //User's Blog
+
+//User's Bio
+
+//User's number of public repositories
+
+//Number of followers on GitHub
+
+//Number of GitHub stars
+
+//Number of people the user is following
